4454bd0e3bf6e62b2995384f4852aa44594340da
Algirdas Čičinskas <algirdas.cicinskas@hostinger.com>
2025-06-12 14:42:36 +0300

    chore: test build

--- generator/docs.go
+++ generator/docs.go
@@ -1,7 +1,7 @@
 package generator
 
 import (
-	"log"
+	"os"
 
 	"github.com/hostinger/api-cli/cmd"
 	"github.com/spf13/cobra/doc"
@@ -10,6 +10,6 @@ import (
 func main() {
 	err := doc.GenMarkdownTree(cmd.RootCmd, "./docs")
 	if err != nil {
-		log.Fatal(err)
+		os.Exit(1)
 	}
 }
